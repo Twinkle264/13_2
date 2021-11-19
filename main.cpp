ece1d6c3546393db5f37bec905359f26c2b724a6
Twinkle264 <twinkle264@gmail.com>
2021-11-19 11:15:05 +0300

    type cost and basket changed to vector

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    float cost[] = {2.5, 4.25, 3.0, 10.0};
-    int basket[] = {1, 1, 0, 3};
+    vector<float> cost = {2.5, 4.25, 3.0, 10.0};
+    vector<int> basket = {1, 1, 0, 3};
 
     float sum = 0.f;
 
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < basket.size(); ++i) {
     sum += cost[basket[i]];
     }
 
